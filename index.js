cc4003b26435aeb1e09660b333ed8b5d5fc7d886
samilalgul <samilalgul@gmail.com>
2020-05-10 03:09:56 +0300

    datas

---
+++ index.js
@@ -0,0 +1,29 @@
+const admin = require('./node_modules/firebase-admin');
+const serviceAccount = require("./serviceAccountKey.json");
+
+const data = require("./data.json");
+const collectionKey = "Barcode"; //name of the collection
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://leadmarkt-28cb3.firebaseio.com"
+});
+
+
+const firestore = admin.firestore();
+const settings = {timestampsInSnapshots: true};
+firestore.settings(settings);
+
+if (data && (typeof data === "object")) {
+Object.keys(data).forEach(docKey => {
+ firestore
+ .collection(collectionKey)
+ .doc(docKey)
+ .set(data[docKey])
+ .then((res) => {
+    console.log("Document " + docKey + " successfully written!");
+}).catch((error) => {
+   console.error("Error writing document: ", error);
+});
+});
+}
